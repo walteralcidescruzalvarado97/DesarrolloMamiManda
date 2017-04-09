cca23446c0f626e38021ea3b5ba560e7f3c2ac3c
Erick Romero <romero.gallardo97@gmail.com>
2017-04-09 13:07:38 -0600

    Splash_BakerySystem

---
+++ MamiManda/MamiManda/Splash_BakerySystem.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Splash_BakerySystem
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Splash_BakerySystem))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.LblInitial = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Controls.Add(Me.LblInitial)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(797, 419)
+        Me.Panel1.TabIndex = 3
+        '
+        'LblInitial
+        '
+        Me.LblInitial.AutoSize = True
+        Me.LblInitial.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblInitial.Location = New System.Drawing.Point(200, 291)
+        Me.LblInitial.Name = "LblInitial"
+        Me.LblInitial.Size = New System.Drawing.Size(65, 16)
+        Me.LblInitial.TabIndex = 0
+        Me.LblInitial.Text = "Initializing"
+        '
+        'Splash_BakerySystem
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkOrange
+        Me.ClientSize = New System.Drawing.Size(797, 419)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Splash_BakerySystem"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Splash_BakerySystem"
+        Me.TransparencyKey = System.Drawing.Color.DarkOrange
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents LblInitial As Label
+End Class
---
+++ MamiManda/MamiManda/Splash_BakerySystem.vb
@@ -0,0 +1,47 @@
+﻿Public Class Splash_BakerySystem
+    Dim i As Integer = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If i = 0 Then
+            LblInitial.Text = "Initializing."
+        ElseIf i = 1 Then
+            LblInitial.Text = "Initializing.."
+        ElseIf i = 2 Then
+            LblInitial.Text = "Initializing..."
+        ElseIf i = 3 Then
+            LblInitial.Text = "Initializing."
+        ElseIf i = 4 Then
+            LblInitial.Text = "Initializing.."
+        ElseIf i = 5 Then
+            LblInitial.Text = "Initializing..."
+        ElseIf i = 6 Then
+            LblInitial.Text = "Initializing."
+        ElseIf i = 7 Then
+            LblInitial.Text = "Initializing.."
+        ElseIf i = 8 Then
+            LblInitial.Text = "Initializing..."
+            Timer1.Interval = 1000
+        ElseIf i = 9 Then
+            LblInitial.Text = "C:\Program Files\IIS Express\AppServer\root\ServerFile_Connection"
+        ElseIf i = 10 Then
+            LblInitial.Text = "C:\Program Files\IIS Express\AppServer\root\Server_BakerySystem"
+        ElseIf i = 11 Then
+            LblInitial.Text = "C:\BakerySystem_Database\Connection-successfully"
+            Timer1.Interval = 500
+        ElseIf i = 12 Then
+            LblInitial.Text = "C:\BakerySystem\Program\Download.File\Starter"
+        ElseIf i = 13 Then
+            LblInitial.Text = "C:\BakerySystem\Program\Download.File\Runnig"
+        ElseIf i = 14 Then
+            LblInitial.Text = "C:\Enable\cmd\BakerySystem.exe"
+        ElseIf i = 15 Then
+            LblInitial.Text = "C:\Load:File\Interfaces.\Login"
+        ElseIf i = 16 Then
+            LblInitial.Text = "Load Complete. Open Program"
+        ElseIf i = 20 Then
+            FrmLogin.Show()
+            Me.Close()
+        End If
+
+        i += 1
+    End Sub
+End Class
